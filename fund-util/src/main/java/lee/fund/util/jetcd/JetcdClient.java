ad9e1f7c09328cf526f106f68f2f0a9815088b34
zhu.li <zhu.li@mtime.com>
2019-09-01 22:20:44 +0800

    mile stone

--- fund-util/src/main/java/lee/fund/util/jetcd/JetcdClient.java
+++ fund-util/src/main/java/lee/fund/util/jetcd/JetcdClient.java
@@ -12,6 +12,7 @@ import com.coreos.jetcd.options.WatchOption;
 import com.google.common.base.Strings;
 import lee.fund.util.config.GlobalConf;
 import lee.fund.util.execute.NamedThreadFactory;
+import lee.fund.util.execute.Schedule;
 import lee.fund.util.lang.UncheckedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,23 +30,20 @@ import java.util.concurrent.TimeUnit;
  */
 public class JetcdClient {
     private static final Logger logger = LoggerFactory.getLogger(JetcdClient.class);
-    private static JetcdClient instance;
-    private static Object obj = new Object();
+    private static volatile JetcdClient instance;
+    private static volatile boolean isInitSuccess;
     private Client client;
     private long leaseId;//租约id，注册服务到etcd上时，使用该租约
     private final int TTL = 15;
 
     private JetcdClient() {
-        /**
-         * TODO etcd挂了，判断什么异常表示挂了，etcd重启了如何再初始化设置
-         * TODO 重启后，注册的服如何自动注册到etcd，最好不要手工启动服务来注册
-         */
-        init();
+        this.connectEtcd();
+        Schedule.set(this::connectEtcd, 2000, 2000);
     }
 
     public static JetcdClient getInstance() {
         if (instance == null) {
-            synchronized (obj) {
+            synchronized (JetcdClient.class) {
                 if (instance == null) {
                     instance = new JetcdClient();
                 }
@@ -54,36 +52,35 @@ public class JetcdClient {
         return instance;
     }
 
-    private void init() {
-        if (this.client == null) {
+    private void connectEtcd() {
+        if (!isInitSuccess) {
             String address = GlobalConf.instance().getEtcdAdress();
             if (Strings.isNullOrEmpty(address)) {
                 throw new UncheckedException("etcd.address can't be null or empty");
             }
             try {
                 this.client = Client.builder().endpoints(Arrays.asList(address.split(","))).build();
-                this.getNodes("test");
+                this.client.getKVClient().get(ByteSequence.fromString("test")).get().getKvs();
                 logger.info("etcd client endpoints={} setting success", address);
+                isInitSuccess = true;
             } catch (Exception e) {
-                logger.error(String.format("etcd client endpoints=%s setting error", address), e);
+                this.checkException(e);
+                logger.error("etcd client connection endpoints[{}] error，will try again...", address);
             }
         }
     }
 
-    private Client getClient() {
-        //TODO 挂了之后重启，要验证.这里如何判断呢
-        if (this.client == null) {
-            synchronized (this) {
-                this.init();
-            }
+    private void checkException(Exception e) {
+        if (e.getMessage().contains("io.grpc.StatusRuntimeException: UNAVAILABLE")) {
+            isInitSuccess = false;
         }
-        return this.client;
     }
 
     public void setNode(String key, String value) {
         try {
-            this.getClient().getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value)).get();
+            this.client.getKVClient().put(ByteSequence.fromString(key), ByteSequence.fromString(value)).get();
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("set node for key = [%s]，value=%s error", key, value), e);
         }
     }
@@ -93,8 +90,9 @@ public class JetcdClient {
             this.setLeaseId();//注册临时节点，设置租约
             ByteSequence bsKey = ByteSequence.fromString(key);
             ByteSequence bsVa = ByteSequence.fromString(value);
-            this.getClient().getKVClient().put(bsKey, bsVa, PutOption.newBuilder().withLeaseId(leaseId).withPrevKV().build()).get();
+            this.client.getKVClient().put(bsKey, bsVa, PutOption.newBuilder().withLeaseId(leaseId).withPrevKV().build()).get();
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("set nodes for key = [%s]，value=%s error", key, value), e);
         }
     }
@@ -102,8 +100,9 @@ public class JetcdClient {
     public List<KeyValue> getNodes(String key) {
         List<KeyValue> keyValues;
         try {
-            keyValues = this.getClient().getKVClient().get(ByteSequence.fromString(key)).get().getKvs();
+            keyValues = this.client.getKVClient().get(ByteSequence.fromString(key)).get().getKvs();
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("get nodes by key = [%s] error", key), e);
         }
         return keyValues;
@@ -113,8 +112,9 @@ public class JetcdClient {
         List<KeyValue> keyValues;
         try {
             GetOption getOption = GetOption.newBuilder().withPrefix(ByteSequence.fromString(prefix)).build();
-            keyValues = this.getClient().getKVClient().get(ByteSequence.fromString(prefix), getOption).get().getKvs();
+            keyValues = this.client.getKVClient().get(ByteSequence.fromString(prefix), getOption).get().getKvs();
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("get nodes with prefix = [%s] error", prefix), e);
         }
         return keyValues;
@@ -122,8 +122,9 @@ public class JetcdClient {
 
     public void delNode(String key) {
         try {
-            this.getClient().getKVClient().delete(ByteSequence.fromString(key)).get().getDeleted();
+            this.client.getKVClient().delete(ByteSequence.fromString(key)).get().getDeleted();
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("del node by key = [%s] error", key), e);
         }
     }
@@ -131,8 +132,9 @@ public class JetcdClient {
     public void delNodesWithPrefix(String prefix) {
         try {
             DeleteOption deleteOption = DeleteOption.newBuilder().withPrefix(ByteSequence.fromString(prefix)).build();
-            this.getClient().getKVClient().delete(ByteSequence.fromString(prefix), deleteOption);
+            this.client.getKVClient().delete(ByteSequence.fromString(prefix), deleteOption);
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("del nodes with prefix = [%s] error", prefix), e);
         }
     }
@@ -140,8 +142,9 @@ public class JetcdClient {
     public Watch.Watcher getWatcher(String key) {
         Watch.Watcher watcher;
         try {
-            watcher = this.getClient().getWatchClient().watch(ByteSequence.fromString(key));
+            watcher = this.client.getWatchClient().watch(ByteSequence.fromString(key));
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("get watcher for [%s] error", key), e);
         }
         return watcher;
@@ -151,8 +154,9 @@ public class JetcdClient {
         Watch.Watcher watcher;
         try {
             WatchOption watchOption = WatchOption.newBuilder().withPrefix(ByteSequence.fromString(prefix)).build();
-            watcher = this.getClient().getWatchClient().watch(ByteSequence.fromString(prefix), watchOption);
+            watcher = this.client.getWatchClient().watch(ByteSequence.fromString(prefix), watchOption);
         } catch (Exception e) {
+            this.checkException(e);
             throw new RuntimeException(String.format("get watcher for [%s] error", prefix), e);
         }
         return watcher;
@@ -163,13 +167,14 @@ public class JetcdClient {
             if (this.leaseId <= 0) {
                 synchronized (this) {
                     if (this.leaseId <= 0) {
-                        this.leaseId = this.getClient().getLeaseClient().grant(this.TTL).get(this.TTL, TimeUnit.SECONDS).getID();
+                        this.leaseId = this.client.getLeaseClient().grant(this.TTL).get(this.TTL, TimeUnit.SECONDS).getID();
                         logger.info("setting etcd client leaseId={} success", this.leaseId);
                         this.keepAlive();
                     }
                 }
             }
         } catch (Exception e) {
+            this.checkException(e);
             throw new UncheckedException("setting etcd client leaseId error", e);
         }
     }
@@ -182,10 +187,11 @@ public class JetcdClient {
     private void keepAlive() {
         Executors.newSingleThreadExecutor(new NamedThreadFactory("KeepAlive")).execute(() -> {
             try {
-                Lease.KeepAliveListener listener = this.getClient().getLeaseClient().keepAlive(this.leaseId);
+                Lease.KeepAliveListener listener = this.client.getLeaseClient().keepAlive(this.leaseId);
                 listener.listen();
                 logger.info("keepAlive lease, leaseId={}", this.leaseId);
             } catch (Exception e) {
+                this.checkException(e);
                 logger.info("keepAlive lease, format leaseId={}，error", this.leaseId, e);
             }
         });
